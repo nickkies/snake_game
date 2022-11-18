de4a59ff803387c380ad234e5e91c8c9ce272b15
YoonSung Kim <opqudk@gmail.com>
2022-11-18 15:27:21 +0900

    String 2

--- main.rs
+++ main.rs
@@ -1,19 +1,11 @@
 fn main() {
-    let a = 10;
-    let b = a;
-    let _c = 15; // copy
-    let sum = add(a, b);
-
-    println!("{}", sum);
-
     let message = String::from("Hello");
-    let message_2 = message; // move
+    print_message(message);
 
     // error
     // println!("{}", message);
-    println!("{}", message_2);
 }
 
-fn add(x: u32, y: u32) -> u32 {
-    x + y
-}
+fn print_message(a: String) {
+    println!("{}", a);
+} // drop
