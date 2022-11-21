c489b98c49ca3fa49940d9bd895d009f7b5c9ea1
YoonSung Kim <opqudk@gmail.com>
2022-11-21 10:40:21 +0900

    Dereference

--- main.rs
+++ main.rs
@@ -1,13 +1,8 @@
 fn main() {
-    let mut message = String::from("Hello");
-    let message_3 = &message;
-    println!("{}", message_3);
-    let message_2 = &mut message;
+    let a = 10;
+    let b = &a;
+    let c = &b;
 
-    unpredictable_mutate(message_2);
-    println!("{}", message);
-}
-
-fn unpredictable_mutate(val: &mut String) {
-    val.push_str("_unpredictable");
+    println!("{}", a == *b);
+    println!("{}", a == **c);
 }
