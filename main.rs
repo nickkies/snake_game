b909492b521c3c7decbd928f7bcb1f50d431cbca
YoonSung Kim <opqudk@gmail.com>
2022-11-21 14:24:51 +0900

    Strings

--- main.rs
+++ main.rs
@@ -1,23 +1,11 @@
 fn main() {
-    let a = 10;
-    let b = &a;
-    let c = &b;
-    let d = b;
-    let mut _e = &b;
-    let f = &&100;
+    let mut message = String::from("Hello");
+    let message_3 = message.clone();
 
-    _e = f;
+    // let slice = &message[2..=4];
 
-    println!("Value of a: {:p}", &a);
-    println!("Value of b: {:p}", b);
-    println!("Value of c: {:p}", c);
-    println!("Value of d: {:p}", d);
+    message.clear();
 
-    println!("Value of e: {:p}", _e);
-    println!("Value of f: {:p}", f);
-    println!("Address of 100: {:p}", &(**f));
-
-    println!("Value of e: {:p}", *_e);
-    println!("Value of f: {:p}", *f);
-    println!("Address of 100: {:p}", &(**_e));
+    println!("message: {}", message);
+    println!("message_3: {}", message_3);
 }
