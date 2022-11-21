292f39ffdf1b2c1aebf35e8d93bbc8e6ee4084c9
YoonSung Kim <opqudk@gmail.com>
2022-11-21 17:26:21 +0900

    Struct

--- main.rs
+++ main.rs
@@ -1,6 +1,59 @@
-fn main() {
-    let _num = 32; // stack
-    let num_2 = Box::new(100); // heap
+struct Person {
+    name: String,
+    last_name: String,
+    age: u32,
+}
 
-    println!("{}", num_2);
+impl Person {
+    fn new() -> Person {
+        Person {
+            name: "Default".to_string(),
+            last_name: "Default".to_string(),
+            age: 0,
+        }
+    }
+
+    fn from(name: String, last_name: String, age: u32) -> Person {
+        Person {
+            name,
+            last_name,
+            age,
+        }
+    }
+
+    // associated function
+    fn some_function() {
+        println!("some_function");
+    }
+
+    // method
+    // first parameter is always self, which repersents the instance of the struct the
+    // method is being called on
+    // Within an impl block, the type Self is an alias for the current type
+    fn display_age(&self) {
+        println!("Current Age: {}", self.age);
+    }
+
+    fn change_age(&mut self, new_age: u32) {
+        self.age = new_age;
+    }
+}
+
+fn main() {
+    Person::some_function();
+
+    let person = Person {
+        name: "Nick".to_string(),
+        last_name: "Kim".to_string(),
+        age: 31,
+    };
+
+    let person_2 = Person::new();
+    let mut person_3 = Person::from(String::from("Nick2"), String::from("Kim"), 6);
+
+    person_2.display_age();
+
+    person_3.change_age(7);
+
+    println!("{}, {} , {}", person.name, person.last_name, person.age);
 }
