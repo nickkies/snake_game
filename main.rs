ae943cd54842ffe131371a5eadd2a5aad078e7a1
YoonSung Kim <opqudk@gmail.com>
2022-11-21 14:10:16 +0900

    Dereference2

--- main.rs
+++ main.rs
@@ -2,7 +2,22 @@ fn main() {
     let a = 10;
     let b = &a;
     let c = &b;
+    let d = b;
+    let mut _e = &b;
+    let f = &&100;
 
-    println!("{}", a == *b);
-    println!("{}", a == **c);
+    _e = f;
+
+    println!("Value of a: {:p}", &a);
+    println!("Value of b: {:p}", b);
+    println!("Value of c: {:p}", c);
+    println!("Value of d: {:p}", d);
+
+    println!("Value of e: {:p}", _e);
+    println!("Value of f: {:p}", f);
+    println!("Address of 100: {:p}", &(**f));
+
+    println!("Value of e: {:p}", *_e);
+    println!("Value of f: {:p}", *f);
+    println!("Address of 100: {:p}", &(**_e));
 }
