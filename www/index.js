924d5dc9c94c5fe546c2a9b76bff2b76540749a8
YoonSung Kim <opqudk@gmail.com>
2022-11-30 17:35:48 +0900

    Import to wasm

--- www/index.js
+++ www/index.js
@@ -1,10 +1,21 @@
 async function init() {
+  const importObject = {
+    console: {
+      log: () => {
+        console.log('Just logging semething!');
+      },
+      error: () => {
+        console.log('I am just error');
+      },
+    },
+  };
+
   const res = await fetch('sum.wasm');
   const buffer = await res.arrayBuffer();
   const byteArray = new Int8Array(buffer);
 
   // debugger;
-  const wasm = await WebAssembly.instantiate(byteArray.buffer);
+  const wasm = await WebAssembly.instantiate(byteArray.buffer, importObject);
   const sumFunction = wasm.instance.exports.sum;
   const result = sumFunction(100, 15);
 
