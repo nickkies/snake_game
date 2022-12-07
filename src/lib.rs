b2a742e091b9d8fef952c8942eba40fb1aeee1bc
YoonSung Kim <opqudk@gmail.com>
2022-12-07 16:54:35 +0900

    Move in one row

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,26 @@ use wee_alloc::WeeAlloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
+#[derive(PartialEq)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
 struct SnakeCell(usize);
 
 struct Snake {
     body: Vec<SnakeCell>,
+    direction: Direction,
 }
 
 impl Snake {
     fn new(spawn_index: usize) -> Snake {
         Snake {
             body: vec![SnakeCell(spawn_index)],
+            direction: Direction::Left,
         }
     }
 }
@@ -22,19 +32,15 @@ impl Snake {
 #[wasm_bindgen]
 pub struct World {
     width: usize,
-    size: usize,
     snake: Snake,
 }
 
 #[wasm_bindgen]
 impl World {
     pub fn new(width: usize) -> World {
-        let size = width * width;
-
         World {
             width,
-            size,
-            snake: Snake::new(rand::thread_rng().gen_range(0..size)),
+            snake: Snake::new(rand::thread_rng().gen_range(0..width * width)),
         }
     }
 
@@ -47,7 +53,30 @@ impl World {
     }
 
     pub fn update(&mut self) {
-        self.snake.body[0].0 = (self.snake_head_idx() + 1) % self.size;
+        let snake_idx = self.snake_head_idx();
+        let (row, col) = self.index_to_cell(snake_idx);
+
+        let (row, col) = match self.snake.direction {
+            Direction::Up => ((row - 1) % self.width, col),
+            Direction::Right => (row, (col + 1) % self.width),
+            Direction::Left => (row, (col - 1) % self.width),
+            Direction::Down => ((row + 1) % self.width, col),
+        };
+
+        let next_idx = self.cell_to_index(row, col);
+        self.set_snake_head(next_idx);
+    }
+
+    fn set_snake_head(&mut self, idx: usize) {
+        self.snake.body[0].0 = idx;
+    }
+
+    fn index_to_cell(&self, idx: usize) -> (usize, usize) {
+        (idx / self.width, idx % self.width)
+    }
+
+    fn cell_to_index(&self, row: usize, col: usize) -> usize {
+        (row * self.width) + col
     }
 }
 
